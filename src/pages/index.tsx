a23b95823644b1a751581362493449489c644c90
hloth <gitlab@hloth.dev>
2023-04-15 19:19:32 +0400

    CardButton features, background assets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import CardButton from '@/shared/ui/card-button'
 import Headline from '@/entities/typography/headline'
-import Button from '@x5io/flat-uikit/dist/Button'
-import Input from '@x5io/flat-uikit/dist/Input'
+import Button from '@x5io/flat-uikit/dist/button'
+import Input from '@x5io/flat-uikit/dist/input'
+import Checkbox from '@x5io/flat-uikit/dist/checkbox'
 
 export default function Home() {
   return (
@@ -16,17 +17,31 @@ export default function Home() {
       />
       <Headline color='rgba(138, 172, 251, 1)'>уровень <strong>стресса</strong> в вашей жизни</Headline>
       <Button>Foobar</Button>
-      <Input 
-        label='Ваш возраст'
-        placeholder='Введите ваш возраст'
-        onEnter={() => {}}
-      />
-      <Input 
-        label='Ваш возраст'
-        placeholder='Введите ваш возраст'
-        value={'33'}
-        onEnter={() => {}}
-      />
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 30, margin: 100 }}>
+        <Input 
+          label='Ваш возраст'
+          placeholder='Введите ваш возраст'
+          onEnter={() => {}}
+        />
+        <Input 
+          label='Ваш возраст'
+          placeholder='Введите ваш возраст'
+          value={'33'}
+          onEnter={() => {}}
+        />
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 30, margin: 100 }}>
+        <Checkbox
+          name='scam'
+        >
+          Нажимая "Оплатить" я подтверждаю свое согласие на постоянные списания скам обман со своей карты 199 рублей в месяц
+        </Checkbox>
+        <Checkbox
+          name='scam2'
+        >
+          Я подтверждаю, что прочитал условия соглашения с использованием ui кита и умею устанавливать компоненты из регистра npm
+        </Checkbox>
+      </div>
     </>
   )
 }
