d6cc7f44f176b0bfecc078c6891e01fcd1a3360c
hloth <gitlab@hloth.dev>
2023-04-16 22:19:30 +0400

    Mobile styles

--- src/widgets/quiz-page/question/ui/content/index.tsx
+++ src/widgets/quiz-page/question/ui/content/index.tsx
@@ -1,6 +1,7 @@
 import { quizQuestions } from '@/widgets/quiz-page/question/model/questions'
 import styles from './styles.module.scss'
 import Headline from '@/entities/typography/headline'
+import CardButton from '@/shared/ui/card-button'
 
 export default function QuestionContent({ questionNumber }: { questionNumber: number }) {
   const question = quizQuestions[questionNumber - 1]
@@ -13,9 +14,16 @@ export default function QuestionContent({ questionNumber }: { questionNumber: nu
         >
           <span dangerouslySetInnerHTML={{ __html: question.text }} />
         </Headline>
+        <span className={styles.hint}>Выберите вариант который больше всего вам соответствует</span>
       </div>
       <div className={styles.content}>
-
+        {question.options.map(question => (
+          <CardButton 
+            key={question.key}
+            title={question.label}
+            caption={question.caption}
+          />
+        ))}
       </div>
     </div>
   )
