996041eed733f7ab71cc17ec797fd5b78507ae57
hloth <gitlab@hloth.dev>
2023-04-21 21:54:08 +0400

    Handle

--- src/shared/range/index.tsx
+++ src/shared/range/index.tsx
@@ -1,5 +1,7 @@
+import React from 'react'
 import { useSpring, animated } from '@react-spring/web'
 import styles from './styles.module.scss'
+import Image from 'next/image'
 
 function Container({ values, valueKey, onChange, defaultValueKey }: {
   values: { key: string, label: string, caption?: string, color: string, icon: import('next/image').StaticImageData }[],
@@ -7,10 +9,13 @@ function Container({ values, valueKey, onChange, defaultValueKey }: {
   defaultValueKey: string
   onChange: () => any
 }) {
+  const [handlePressed, setHandlePressed] = React.useState(false)
+
   const defaultValue = values.find(v => v.key === defaultValueKey)
   if (!defaultValue) throw new Error('No default value provided to Range component')
 
   const value = values.find(v => v.key === valueKey) ?? defaultValue
+  const valueIndex = values.findIndex(v => v.key === value.key)
 
   const captionStyles = useSpring({
     color: value.color
@@ -19,11 +24,33 @@ function Container({ values, valueKey, onChange, defaultValueKey }: {
   const slideStyles = { 
     background: `linear-gradient(to right, ${values.map(v => v.color).join(', ')})`
   }
+
+  const handleStyles = useSpring({
+    left: `${valueIndex/values.length*100}%`,
+    scale: handlePressed ? 1.1 : 1
+  })
   
   return (
     <div className={styles.container}>
       <animated.span className={styles.label} style={captionStyles}>{value.label}</animated.span>
-      <div className={styles.slide} style={slideStyles} />
+      <div className={styles.slideContainer}>
+        <div className={styles.slide} style={slideStyles} />
+        <animated.div
+          className={styles.handle} 
+          style={handleStyles}
+          onPointerDown={() => setHandlePressed(true)}
+          onPointerUp={() => setHandlePressed(false)}
+          onPointerLeave={() => setHandlePressed(false)}
+        >
+          <Image 
+            src={value.icon} 
+            width={40} height={40} 
+            alt='Индикатор (перетягивайте)' 
+            quality={100}
+            draggable={false}
+          />
+        </animated.div>
+      </div>
       <div className={styles.ticks}>
 
       </div>
