55486948978ac5fe22095f282293dbbfba7095d7
hloth <gitlab@hloth.dev>
2023-06-13 18:21:29 +0400

    Discount timer in last section of results page

--- src/features/quiz-results/timer/index.tsx
+++ src/features/quiz-results/timer/index.tsx
@@ -1,11 +1,35 @@
+import React from 'react'
 import styles from './styles.module.scss'
 import Clock from './clock.svg'
 
-export function Timer() {
+export function Timer({ from }: {
+  from: Date
+}) {
+  const [secondsLeft, setSecondsLeft] = React.useState(0)
+
+  // const hours = secondsLeft / 3600 % 3600 ^ 0
+  const minutes = secondsLeft / 60 % 60 ^ 0
+  const seconds = ('0' + secondsLeft % 60).slice(-2)
+
+  React.useEffect(() => {
+    setSecondsLeft((from.getTime() - Date.now()) / 1000 ^ 0)
+  }, [from])
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (secondsLeft <= 0) {
+        setSecondsLeft(60 * 15)
+      } else {
+        setSecondsLeft(secondsLeft - 1)
+      }
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [secondsLeft])
+
   return (
     <div className={styles.timer}>
       <Clock />
-      <span>Акция закончится через <strong>{'12:32'}</strong></span>
+      <span>Акция закончится через <strong>{`${minutes}:${seconds}`}</strong></span>
     </div>
   )
 }
--- src/widgets/quiz-results/last-section/index.tsx
+++ src/widgets/quiz-results/last-section/index.tsx
@@ -1,12 +1,15 @@
+import React from 'react'
 import Paragraph from '@/entities/typography/paragraph'
 import styles from './styles.module.scss'
 import GetPlan from '@/features/quiz-results/get-plan'
 import { Timer } from '@/features/quiz-results/timer'
 
 export function LastSection() {
+  const date = React.useMemo(() => new Date(Date.now() + 1000*60*15), [])
+
   return (
     <section className={styles.lastSection}>
-      <Timer />
+      <Timer from={date} />
       <span className={styles.title}>Мы сможем помочь вам улучшить ваше состояние уже за 3 дня</span>
       <Paragraph>Не откладывайте на потом то, что можно начать сейчас</Paragraph>
       <GetPlan>Получить план за 27 рублей</GetPlan>
